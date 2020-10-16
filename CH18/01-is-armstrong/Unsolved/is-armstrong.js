707b53e025c4949fedf4038e847e9e64566dbd20
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-10-16 14:18:48 -0400

    chapter 18 added

---
+++ CH18/01-is-armstrong/Unsolved/is-armstrong.js
@@ -0,0 +1,12 @@
+// Write a function that takes a positive integer and returns true if the integer is an armstrong number, else return false. To find out if a number is an armstrong number, take each individual digit and raise it to the power of the length of the entire number and add the digits. If the sum equals the original number, the number is an armstrong number
+
+var isArmstrong = (num) => {
+  let str = String(num);
+  let sum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let digit = parseInt(str[i]);
+    sum += digit ** str.length;
+  }
+  return sum === num;
+};
